4946737e23ce71cfb773f577e0ce19a1058ca3e0
SakonphatQ <sakonphat999@gmail.com>
2025-04-18 15:08:15 +0700

    Update manu

--- app/(Profile)/profile/page.jsx
+++ app/(Profile)/profile/page.jsx
@@ -9,7 +9,7 @@ const UserProfile = () => {
             <h1 className="text-3xl font-bold text-center mt-10">ชื่อผู้ใช้</h1>
             <div className="py-4"/>
             {/* รูปโปรไฟล์ */}
-            <div className="w-32 h-32">
+            <div className="w-32 h-32 rounded-full overflow-hidden border-4 border-blue-500 shadow-lg">
                 <Avatar className="w-full h-full">
                     <AvatarImage src='https://github.com/shacn.png' alt='@shadcn' />
                     <AvatarFallback className='text-black'>BT</AvatarFallback>
@@ -17,17 +17,17 @@ const UserProfile = () => {
             </div>
 
             {/* ปุ่มต่าง ๆ */}
-            <div className="mt-20 flex flex-col gap-3">
-                <a href='/Setting' className="px-6 py-2 bg-blue-500 text-white rounded-lg shadow-md text-center">
+            <div className="mt-16 flex flex-col gap-4">
+                <a href='/Setting' className="px-6 py-3 bg-blue-500 text-white rounded-lg shadow-md text-center">
                     แก้ไขข้อมูล
                 </a>
-                <a href='/SUBSCRIPTION' className="px-6 py-2 bg-yellow-400 text-black font-bold rounded-lg shadow-md text-center">
+                <a href='/SUBSCRIPTION' className="px-6 py-3 bg-yellow-400 text-black font-bold rounded-lg shadow-md text-center">
                     SUBSCRIPTION
                 </a>
-                <a href='/HowTO' className="px-6 py-2 bg-blue-500 text-white rounded-lg shadow-md text-center">
+                <a href='/HowTO' className="px-6 py-3 bg-blue-500 text-white rounded-lg shadow-md text-center">
                     วิธีใช้งาน
                 </a>
-                <a href='/auth' className="px-6 py-2 bg-red-500 text-white rounded-lg shadow-md text-center">
+                <a href='/auth' className="px-6 py-3 bg-red-500 text-white rounded-lg shadow-md text-center">
                     ออกจากระบบ
                 </a>
             </div>
